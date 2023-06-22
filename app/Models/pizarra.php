fe6745bec44906fdd701ef121236f9e6fd16a6a2
Nataliaqg <nataliaquirogag@gmail.com>
2023-06-21 22:59:33 -0400

    cambios a migracion

--- app/Models/pizarra.php
+++ app/Models/pizarra.php
@@ -8,6 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class pizarra extends Model
 {
     use HasFactory;
+    protected $table='pizarras';
+    protected $guarded=[
+        'id',
+        'created_at',
+        'updated_at',
+    ];
 
     public function user(){
         return $this->belongsTo(User::class);
